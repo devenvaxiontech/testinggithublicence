4e42c6fe6b3b35e84d5d4a83fa276ad83c2feb15
musadiq012 <developerenvaxiontech@gmail.com>
2021-12-20 16:36:45 +0500

    Update test.php

--- test.php
+++ test.php
@@ -3,8 +3,8 @@
 * Plugin Name: Check Ud github and licence  
 * Plugin URI: https://www.facebook.com/musadiq.khan.tareen
 * Description: This is the very first plugin I ever created.
-* Version: 1.013
-* Author: Test taker
+* Version: 1.1
+* Author: Musadiq Mehmood
 * Author URI: https://www.facebook.com/musadiq.khan.tareen
 **/
 
